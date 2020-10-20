94f5d779bf520087cd70d38843df94de2f40f5af
Gabriel Junior <gabrieljuniorkdc@gmail.com>
2020-10-20 19:40:22 -0300

    Commit class 47. Closes #2. - Adicionando os POJOs

---
+++ src/main/java/guru/springframework/sfgpetclinic/model/Person.java
@@ -0,0 +1,22 @@
+package guru.springframework.sfgpetclinic.model;
+
+public class Person {
+    private String firstname;
+    private String lastName;
+
+    public String getFirstname() {
+        return firstname;
+    }
+
+    public void setFirstname(String firstname) {
+        this.firstname = firstname;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+}
