6b7112b83b6ba69e80608c6625575af085a8bc83
Gabriel Junior <gabrieljuniorkdc@gmail.com>
2020-10-27 18:12:41 -0300

    Adição da classe BaseEntity. Class 51. Closes #10.

--- pet-clinic-data/src/main/java/guru/springframework/sfgpetclinic/services/VetService.java
+++ pet-clinic-data/src/main/java/guru/springframework/sfgpetclinic/services/VetService.java
@@ -5,7 +5,7 @@ import guru.springframework.sfgpetclinic.model.Vet;
 import java.util.Set;
 
 public interface VetService {
-    
+
     public Vet findById(Long id);
     public Vet save(Vet vet);
     public Set<Vet> findAll();
