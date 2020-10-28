d30edfb4f48d36df6703675de0bab496109117ee
Gabriel Junior <gabrieljuniorkdc@gmail.com>
2020-10-28 19:18:57 -0300

    Implement Map Based Services, class 62. Closes #6.

--- pet-clinic-data/src/main/java/guru/springframework/sfgpetclinic/services/CrudService.java
+++ pet-clinic-data/src/main/java/guru/springframework/sfgpetclinic/services/CrudService.java
@@ -3,7 +3,7 @@ package guru.springframework.sfgpetclinic.services;
 import java.util.Set;
 
 public interface CrudService<T, ID> {
-    public Set<T> finalAll();
+    public Set<T> findAll();
     public T findById(ID id);
     public T save(T object);
     public void delete(T object);
