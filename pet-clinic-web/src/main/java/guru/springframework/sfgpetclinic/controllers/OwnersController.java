52c78660f13c9d64f292fc616c8fe01c60541488
Gabriel Junior <gabrieljuniorkdc@gmail.com>
2020-11-03 18:32:39 -0300

    Adding owners page and controller, class 67. Closes #13.

---
+++ pet-clinic-web/src/main/java/guru/springframework/sfgpetclinic/controllers/OwnersController.java
@@ -0,0 +1,15 @@
+package guru.springframework.sfgpetclinic.controllers;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@RequestMapping("/owners")
+@Controller
+public class OwnersController {
+
+    @RequestMapping({"", "/", "/index", "/index.html"})
+    public String listOwners() {
+
+        return "owners/index";
+    }
+}
