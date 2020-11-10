e44bfda3d5c9268b0b3e47f7ee791c0dc7eaa4b2
Gabriel Junior <gabrieljuniorkdc@gmail.com>
2020-11-10 19:10:41 -0300

    Added Vets List and updated controller, class 83. Closes 15#.

--- pet-clinic-web/src/main/java/guru/springframework/sfgpetclinic/bootstrap/DataLoader.java
+++ pet-clinic-web/src/main/java/guru/springframework/sfgpetclinic/bootstrap/DataLoader.java
@@ -24,14 +24,12 @@ public class DataLoader implements CommandLineRunner {
         owner1.setId(1L);
         owner1.setFirstname("Gabriel");
         owner1.setLastName("Junior");
-
         ownerService.save(owner1);
 
         Owner owner2 = new Owner();
         owner2.setId(2L);
         owner2.setFirstname("Michael");
         owner2.setLastName("Jackson");
-
         ownerService.save(owner2);
 
         System.out.println("Loaded Owners...");
@@ -40,14 +38,12 @@ public class DataLoader implements CommandLineRunner {
         vet1.setId(1L);
         vet1.setFirstname("Sam");
         vet1.setLastName("Winchester");
-
         vetService.save(vet1);
 
         Vet vet2 = new Vet();
-        vet1.setId(2L);
-        vet1.setFirstname("Dean");
-        vet1.setLastName("Winchester");
-
+        vet2.setId(2L);
+        vet2.setFirstname("Dean");
+        vet2.setLastName("Winchester");
         vetService.save(vet2);
 
         System.out.println("Loaded Vets...");
